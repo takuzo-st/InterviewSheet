ae53949fe94ed5df9ed3f9db2e30ea5542abaa3a
takuzo-st <stst.jac@gmail.com>
2023-05-14 19:09:39 +0900

    送信後画面の入力内容表示を削除

--- src/dkeeper_frontend/src/components/Pages/InterviewSheet/InterviewSheetSubmitted.jsx
+++ src/dkeeper_frontend/src/components/Pages/InterviewSheet/InterviewSheetSubmitted.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import CheckList from "./CheckList";
-import BasicInfo from "../BasicInfo/BasicInfo";
 
 function InterviewSheetSubmitted() {
   return (
     <div className="interview-sheet-submitted">
       <h1>問診票</h1>
-      <p>以下の内容で送信しました。</p>
-      <BasicInfo />
-      <CheckList />
+      <p>送信しました。</p>
     </div>
   );
 }
