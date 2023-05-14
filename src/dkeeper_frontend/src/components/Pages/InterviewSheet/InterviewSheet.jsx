8bbb33abf3171bfae16f199586fec553f79b5322
takuzo-st <stst.jac@gmail.com>
2023-05-14 19:34:10 +0900

    不要なログ出力のため削除

--- src/dkeeper_frontend/src/components/Pages/InterviewSheet/InterviewSheet.jsx
+++ src/dkeeper_frontend/src/components/Pages/InterviewSheet/InterviewSheet.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import BasicInfo from "../BasicInfo/BasicInfo";
 import CheckList from "./CheckList";
-import { useLocation } from "react-router-dom";
 
 function InterviewSheet() {
-  console.log(useLocation().pathname);
   return (
     <div>
       <BasicInfo />
